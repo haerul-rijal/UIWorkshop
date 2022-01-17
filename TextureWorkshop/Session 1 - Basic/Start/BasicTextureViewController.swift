34186b811ae974f97f124982fa18fd5dffa2ffcf
Haerul Rijal <haerul.rijal@tokopedia.com>
2022-01-17 16:17:45 +0800

    Session 1

--- TextureWorkshop/Session 1 - Basic/Start/BasicTextureViewController.swift
+++ TextureWorkshop/Session 1 - Basic/Start/BasicTextureViewController.swift
@@ -6,7 +6,28 @@
 //
 
 import UIKit
+import AsyncDisplayKit
 
-class BasicTextureViewController: UIViewController {
-   
+class BasicTextureViewController: ASDKViewController<ASDisplayNode> {
+    override init() {
+        super.init(node: ASDisplayNode())
+        node.backgroundColor = .white
+        let subNode = ASDisplayNode()
+        subNode.backgroundColor = .orange
+        subNode.style.preferredSize = CGSize(width: 300, height: 300)
+        node.addSubnode(subNode)
+        node.layoutSpecBlock = { _, _ in
+            return ASCenterLayoutSpec(centeringOptions: .XY,
+                                      sizingOptions: [],
+                                      child: subNode)
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
 }
